29f3ee59c5168edc2874f7511bf53131dab606e2
serii <seriiburduja@gmail.com>
2019-10-02 10:05:32 +0300

    click on progressbar

--- single-video.php
+++ single-video.php
@@ -1,9 +1,10 @@
 <?php
     get_header();
-	/**
-	 * Template Name: Video Page
-	 */
 ?>
+<?php if (have_posts()): ?>
+
+	<?php the_post(); ?>
+
 	<?php $page_bg_image_url = getThePostThumbSrc(1920, 340); ?>
 
 	<div class="finance-single">
@@ -18,26 +19,12 @@
 		<section class="finance-single__container">
 			<?php get_sidebar('categories'); ?>
 			<section class="main-single">
-				<?php
-					$video = new WP_Query([
-						'post_type' => 'video',
-						'posts_per_page' => 1,
-                        'category_video' => 'legumicultura'
-					]);
-				?>
-                <?php if($video->have_posts()): ?>
-                    <?php while($video->have_posts()): ?>
-                        <?php $video->the_post(); ?>
-
-<!--                        <video controls poster="https://html5book.ru/wp-content/themes/papercuts/video/martynko.png" preload="none">-->
-<!--                            <source src="--><?php //echo carbon_get_the_post_meta('crb_video_file'); ?><!--" type="video/mp4">-->
-<!--                        </video>-->
-                    <?php endwhile; ?>
-                    <?php else: ?>
-                <?php endif; ?>
+                <?php the_content(); ?>
             </section>
         </section>
 <!--		--><?php //require_once __DIR__.'/template-parts/connect-now.php';?>
 	</div>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
